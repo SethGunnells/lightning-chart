7e058f86acfa96f2b9c55237cec45f2b91cc4142
Seth Gunnells <sethgunnells@gmail.com>
2016-02-12 14:18:02 -0800

    Upgrade components to functional components
    Closes #13

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,26 +1,26 @@
 import '../styles/default.scss';
-import '../styles/alt.scss';
 import css from "./chart.scss";
 
-import React from 'react';
 import ChartSection from './ChartSection';
 
-export default class Chart extends React.Component {
+////////////////////////////////////////////////////////////////////////////////
 
-  render() {
-    var chart = this.props.chart;
-    var style = this.props.styleName || 'default';
+const Chart = ({
+  chart,
+  options = {}
+}) => {
+  var style = options.styleName || 'default';
 
-    return (
-      <div className={"lc-chart lc-style-" + style + " " + css.chart}>
-        <h1 className="lc-chart-title">{chart.title}</h1>
-        <div className="lc-chart-writers">{chart.writers.join(', ')}</div>
+  return (
+    <div className={"lc-chart lc-style-" + style + " " + css.chart}>
+      <h1 className="lc-chart-title">{chart.title}</h1>
+      <div className="lc-chart-writers">{chart.writers.join(', ')}</div>
 
-        {chart.sections.map(function(section) {
-          return <ChartSection section={section}/>;
-        })}
-      </div>
-    );
-  }
+      {chart.sections.map(function(section) {
+        return <ChartSection section={section}/>;
+      })}
+    </div>
+  );
+};
 
-}
+export default Chart;
--- src/components/ChartSection.jsx
+++ src/components/ChartSection.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
 import ChartLine from './ChartLine';
 
-export default class ChartSection extends React.Component {
+////////////////////////////////////////////////////////////////////////////////
 
-  render() {
-    var section = this.props.section;
+const ChartSection = ({
+  section
+}) => {
 
-    var title = null;
-    if (section.title) {
-      title = (
-        <h2 className="lc-chart-section-title">
-          {section.title.toUpperCase()}
-        </h2>
-      );
-    }
-
-    return (
-      <div className="lc-chart-section">
-        {title}
-        <div>
-          {section.lines.map(function(line) {
-            return <ChartLine line={line}/>;
-          })}
-        </div>
-      </div>
+  var title = null;
+  if (section.title) {
+    title = (
+      <h2 className="lc-chart-section-title">
+        {section.title.toUpperCase()}
+      </h2>
     );
   }
 
-}
+  return (
+    <div className="lc-chart-section">
+      {title}
+      <div>
+        {section.lines.map(function(line) {
+          return <ChartLine line={line}/>;
+        })}
+      </div>
+    </div>
+  );
+
+};
